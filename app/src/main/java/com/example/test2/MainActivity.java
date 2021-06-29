02b5c027ca5cf6d1f2801d187ba4dc2ab8949413
Priyanshu Arya <priyanshuarya22@gmail.com>
2021-06-29 13:07:00 +0530

    Commit 1

--- app/src/main/java/com/example/test2/MainActivity.java
+++ app/src/main/java/com/example/test2/MainActivity.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    //initialising different views
     Button b1, b2, b3;
     AlertDialog ad;
     Dialog d;
@@ -20,49 +21,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //assigning buttons
         b1 = findViewById(R.id.b1);
         b2 = findViewById(R.id.b2);
         b3 = findViewById(R.id.b3);
+        //setting listener for button 1
         b1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //creating toast
                 Toast.makeText(MainActivity.this, "This is a toast.", Toast.LENGTH_SHORT).show();
             }
         });
+        //setting listener for button 2
         b2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //building a alert dialog
                 AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+                //setting title for alert dialog
                 b.setTitle("Alert Message");
+                //setting message for alert dialog
                 b.setMessage("This is a test alert message. Press Yes to close the application and no to close this window.");
+                //setting positive response
                 b.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        //closing the application
                         MainActivity.this.finish();
                     }
                 });
+                //setting negative response
                 b.setNegativeButton("No", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        //closing dialog box
                         ad.cancel();
                     }
                 });
+                //creating alert dialog
                 ad = b.create();
+                //showing dialog
                 ad.show();
             }
         });
+        //setting listener for button 3
         b3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //creating a new dialog box
                 d = new Dialog(MainActivity.this);
+                //setting a view for dialog box
                 d.setContentView(R.layout.customise_dialog);
+                //assigning button
                 Button b = d.findViewById(R.id.c1);
+                //setting listener to button
                 b.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        //exiting the dialog box
                         d.cancel();
                     }
                 });
+                //showing the dialog box
                 d.show();
             }
         });
